0be42c63eb69a5f041912c83bcf2e87931033372
Andrew Hanes <ahanes@csh.rit.edu>
2014-10-23 21:33:22 -0400

    add user api call

--- webnews/webnews.py
+++ webnews/webnews.py
@@ -12,13 +12,16 @@ API_KEY = open("private/apikey").read()
 WEBNEWS_BASE = "https://webnews.csh.rit.edu/"
 API_AGENT = "webnews-python"
 
-def GET(action, args={}):
+def GET(action, args={}, api_key = API_KEY):
     action = action.value
-    args['api_key'] = API_KEY
+    args['api_key'] = api_key
     args['api_agent'] = API_AGENT
     args = urllib.parse.urlencode(args)
     req = urllib.request.urlopen(WEBNEWS_BASE + action + '?' + args).readall().decode('utf-8')
     return json.loads(req)
 
-resp = GET(Actions.user)
+def user(api_key = API_KEY):
+    return GET(Actions.user)
+
+resp = user()
 print(resp)
