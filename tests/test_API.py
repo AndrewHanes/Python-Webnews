47cd0506cef79309556cbaa09ccbddd24349c1d1
Andrew Hanes <ahanes@csh.rit.edu>
2014-10-26 23:13:04 -0400

    oops

--- tests/test_API.py
+++ tests/test_API.py
@@ -1,11 +1,9 @@
 from unittest import TestCase
-
-__author__ = 'ahanes'
-
+from webnews import api
 
 class TestAPI(TestCase):
     def test_POST(self):
-        self.fail()
+        self.api = api.API(open("private/apikey").read())
 
     def test_GET(self):
         self.fail()
