cd32952f5e2f6eb6c0cf03546ee04f19ccd487a8
Andrew Hanes <ahanes@csh.rit.edu>
2014-10-26 22:59:10 -0400

    tests

---
+++ tests/test_API.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+__author__ = 'ahanes'
+
+
+class TestAPI(TestCase):
+    def test_POST(self):
+        self.fail()
+
+    def test_GET(self):
+        self.fail()
+
+    def test_unread_counts(self):
+        self.fail()
+
+    def test_newsgroups(self):
+        self.fail()
+
+    def test_newsgroups_search(self):
+        self.fail()
+
+    def test_search(self):
+        self.fail()
+
+    def test_post_specifics(self):
+        self.fail()
