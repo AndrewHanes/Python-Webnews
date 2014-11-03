b43a790a28c6664d8a015b5c26dcb21598d79d13
Andrew Hanes <ahanes@csh.rit.edu>
2014-11-02 21:47:01 -0500

    add newsgroup object

--- webnews/webnews.py
+++ webnews/webnews.py
@@ -1,4 +1,5 @@
 from webnews import api
+import json
 
 class WebnewsObject(object):
     def __init__(self, api_val):
@@ -7,4 +8,17 @@ class WebnewsObject(object):
         else:
             self._api = api.API(api_val)
 
+    def populate(self, ng):
+        for k in ng:
+            setattr(self, k, ng[k])
 
+class Newsgroup(WebnewsObject):
+
+    #For autocomplete in IDE mostly.  Not needed to compile
+    unread_class, status, updated_at, created_at, name, newest_date, unread_count = ([None for i in range(7)])
+
+    def __init__(self, api_val, name):
+        super(Newsgroup, self).__init__(api_val)
+        ng = self._api.newsgroups_search(name)
+        self.populate(ng['newsgroup'])
+        print([i for i in ng['newsgroup'].keys()])
