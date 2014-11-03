2a8579a4b509a2f9f4ac8228a582b98da2e58950
Andrew Hanes <ahanes@csh.rit.edu>
2014-11-02 21:18:12 -0500

    add webnews object

--- webnews/webnews.py
+++ webnews/webnews.py
@@ -1,3 +1,10 @@
-class Post:
-    def __init__(self):
-        pass
+from webnews import api
+
+class WebnewsObject(object):
+    def __init__(self, api_val):
+        if type(api) == api.API:
+            self._api = api_val
+        else:
+            self._api = api.API(api_val)
+
+
